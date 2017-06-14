f26adb404c68c108635f082f7fa71005bcf62387
anna <d30418ak@gmail.com>
2017-06-14 17:30:49 +0900

    remove log

--- library/src/main/java/com/baoyz/widget/PullRefreshLayout.java
+++ library/src/main/java/com/baoyz/widget/PullRefreshLayout.java
@@ -6,7 +6,6 @@ import android.graphics.Color;
 import android.support.v4.view.MotionEventCompat;
 import android.support.v4.view.ViewCompat;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.MotionEvent;
 import android.view.View;
@@ -513,7 +512,6 @@ public class PullRefreshLayout extends ViewGroup {
 
         mTarget.layout(left, top + mTarget.getTop(), left + width - right, top + height - bottom + mTarget.getTop());
         mRefreshView.layout(left, top, left + width - right, top + height - bottom);
-        Log.e(TAG, "onLayout:");
     }
 
     private int dp2px(int dp) {
